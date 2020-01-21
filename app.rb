0df3e510cc378eea2c249c8313bd36e2882cfb8b
Colman Currie <curriecolman@gmail.com>
2020-01-20 21:28:37 -0800

    update app.rb

--- app.rb
+++ app.rb
@@ -1,124 +1,144 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/volunteer')
 require('./lib/project')
 require('pry')
-also_reload('lib/**/*.rb')
-require('./lib/volunteer')
 require("pg")
 
-DB =PG.connect({:dbname => "volunteer_tracker"})
+also_reload('lib/**/*.rb')
 
-get('/') do
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get ('/') do
   @projects = Project.all
-  erb(:projects)
+  @volunteers = Volunteer.all
+  erb(:home)
 end
 
-get('/projects') do
-  if params["clear"]
-    @projects = Project.clear()
-  elsif params["search_input"]
-    @projects = Project.search(params["search_input"])
-  elsif params["sort_list"]
-    @projects = Project.sort()
-
-  else
-    @projects = Project.all
-  end
-  erb(:projects)
+get ('/home') do
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:home)
 end
 
-get('/projects/new') do
+get('/project/new') do
   erb(:new_project)
 end
 
-post('/projects') do
-  title = params[:title]
-  project = Project.new(:title => title, :id => nil)
-  project.save()
-  @projects = Project.all()
-  erb(:projects)
-end
 
-get('/projects/:id') do
-  @project = Project.find(params[:id].to_i())
-  erb(:project)
-end
+post ('/home') do
 
-get('/projects/:id/edit') do
-@project = Project.find(params[:id].to_i())
-erb(:edit_project)
-end
-
-patch('/projects/:id') do
-@project  = Project.find(params[:id].to_i())
-@project.update(params[:title])
-@projects = Project.all
-erb(:projects)
-end
-
-delete('/projects/:id') do
-  @project = Project.find(params[:id].to_i())
-  @project.delete()
+if params[:title] && params[:name]
+  name = params[:name]
+  project_id = params[:title]
+  volunteer = Volunteer.new({:name => name, :project_id => project_id ,:id => nil})
+  volunteer.save
   @projects = Project.all
-  erb(:projects)
+  @volunteers = Volunteer.all
+  erb(:home)
+
+elsif params[:title]
+    title = params[:title]
+    project = Project.new({:title => title, :id => nil})
+    project.save
+    @projects = Project.all
+    @volunteers = Volunteer.all
+    erb(:home)
+else
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:home)
+
+
+  end
 end
 
-#Volunteers Routing¡! - -- - -- - - - - - - - >
-
-get('/volunteers/new') do
+get '/volunteer/new' do
+  @projects = Project.all
   erb(:new_volunteer)
 end
 
-get('/volunteers') do
-  if params["clear"]
-    @volunteers = Volunteer.clear()
-  elsif params["search_input"]
-    @volunteers = Volunteer.search(params["search_input"])
-  elsif params["sort_list"]
-    @volunteers = Volunteer.sort()
-  else
-    @volunteers = Volunteer.all
-  end
-  erb(:volunteers)
+
+
+get ('/home/project/:id')do
+
+  @project = Project.find(params[:id].to_i())
+  @volunteers = @project.volunteers()
+
+  erb(:project)
+
 end
 
-get('/volunteers/:id') do
-  @volunteer = Volunteer.find(params[:id].to_i())
-  erb(:volunteer)
+
+get('/home/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
 end
 
-get('/volunteers/:id/edit') do
-@volunteer = Volunteer.find(params[:id].to_i())
-erb(:edit_volunteer)
-end
 
-post('/volunteers') do
-  name = params[:name]
-  volunteer = Volunteer.new(:name => name, :id => nil)
-  volunteer.save()
-  @volunteers = Volunteer.all()
-  erb(:volunteers)
-end
+delete('/home/projects/:id')do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
 
-patch('/volunteers/:id') do
-  @volunteer  = Volunteer.find(params[:id].to_i())
-  @volunteer.update(params[:name])
+  @projects = Project.all
   @volunteers = Volunteer.all
-  erb(:volunteers)
+
+  erb(:home)
 end
 
-delete('/volunteers/:id') do
+
+
+patch ('/home/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update({:title => params[:title], :id => nil})
+  @volunteers = Volunteer.all
+  @projects = Project.all
+
+  erb(:home)
+end
+
+
+
+#Volunteers Routing¡! - -- - -- - - - - - - - >
+
+
+
+
+
+
+get ('/home/volunteers/:id')do
+
+  @volunteer = Volunteer.find(params[:id].to_i())
+
+  erb(:volunteer)
+
+end
+
+
+delete('/home/volunteers/:id')do
   @volunteer = Volunteer.find(params[:id].to_i())
   @volunteer.delete()
+
+  @projects = Project.all
   @volunteers = Volunteer.all
-  erb(:volunteers)
+
+  erb(:home)
 end
 
-post ('/volunteers/:id/projects') do
+
+patch ('/home/volunteers/:id') do
   @volunteer = Volunteer.find(params[:id].to_i())
-  project_name = params[:project_name]
-  # project = Project.new({:name => project_name, :id => nil})
-  # project.save()
-  @volunteer.update({:project_name => project_name})
+  @volunteer.update({:name => params[:name], :id => nil})
+  @volunteers = Volunteer.all
+  @projects = Project.all
+
+  erb(:home)
+end
+
+get ('/home/volunteers/:id/edit')do
+
+  @volunteer = Volunteer.find(params[:id].to_i())
+
   erb(:edit_volunteer)
+
 end
